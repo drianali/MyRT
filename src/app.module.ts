13cdedf7fa4600e4a7d6c77eb1f14d83faa6efb3
revalinasalsabila <revalinasalsabila20@gmail.com>
2025-12-06 11:09:44 +0800

    create database announcement

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PeoplesModule } from './peoples/peoples.module';
 import { RolesModule } from './roles/roles.module';
+import { AnnouncementModule } from './announcement/announcement.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, PeoplesModule, RolesModule],
+  imports: [UsersModule, PrismaModule, PeoplesModule, RolesModule, AnnouncementModule],
   controllers: [AppController],
   providers: [AppService],
 })
