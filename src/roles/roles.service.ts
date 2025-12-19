25e4fe0c1fae471e986b55aa84f662c47db151fd
drianali <maulanaalisaputra27@gmail.com>
2025-12-19 13:57:37 +0800

    Update CRUD : Roles

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
-  }
-
-  findAll() {
-    return `This action returns all roles`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
-  }
-
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} role`;
-  }
+  constructor(private prisma: PrismaService) {}
+      
+      create(data: CreateRoleDto) {
+          return this.prisma.role.create({ data });
+        }
+      
+        findAll() {
+          return this.prisma.role.findMany();
+        }
+      
+        findOne(id: number) {
+          return this.prisma.role.findUnique({ where: { id } });
+        }
+      
+        update(id: number, data: UpdateRoleDto) {
+          return this.prisma.role.update({ where: { id }, data });
+        }
+      
+        remove(id: number) {
+          return this.prisma.role.delete({ where: { id } });
+        }
 }
